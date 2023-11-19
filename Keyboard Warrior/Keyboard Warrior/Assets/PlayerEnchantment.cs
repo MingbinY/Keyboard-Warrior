71af1710c12186cfae50f5a4818df59dbaf518bd
Mingbin Yang <yang.mingb@northeastern.edu>
2023-11-19 14:36:29 -0500

    PlayerEnchant

---
+++ Keyboard Warrior/Keyboard Warrior/Assets/PlayerEnchantment.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyboardWarrior
+{
+    public class PlayerEnchantment : EnchantableObject
+    {
+        public override void UpEvent()
+        {
+            base.UpEvent();
+        }
+
+        public override void DownEvent()
+        {
+            base.DownEvent();
+        }
+
+        public override void LeftEvent()
+        {
+            base.LeftEvent();
+        }
+        public override void RightEvent()
+        {
+            base.RightEvent();
+        }
+
+        public override void SpaceEvent()
+        {
+            base.SpaceEvent();
+        }
+
+        public override void BaseEvent()
+        {
+            base.BaseEvent();
+        }
+    }
+}
