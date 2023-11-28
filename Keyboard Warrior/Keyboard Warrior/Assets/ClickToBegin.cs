ca88950555c5ffd3f629e7dc0bef2cdf26435907
Mingbin Yang <yang.mingb@northeastern.edu>
2023-11-27 22:50:05 -0500

    Title Screen

---
+++ Keyboard Warrior/Keyboard Warrior/Assets/ClickToBegin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace KeyboardWarrior
+{
+    public class ClickToBegin : MonoBehaviour
+    {
+        public GameObject textObject;
+
+        private void Start()
+        {
+            StartCoroutine(BlinkText());
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //GameSceneManager.Instance.LoadNextScene();
+            }
+        }
+
+        IEnumerator BlinkText()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                textObject.SetActive(false);
+                yield return new WaitForSeconds(0.5f);
+                textObject.SetActive(true);
+            }
+        }
+    }
+}
